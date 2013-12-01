adf73b2a39d8b9a46b2a62b32d940cf673034352
Graham McGregor <haxxor42@gmail.com>
2013-11-30 23:33:58 -0500

    formatting & renamed variables

--- login.js
+++ login.js
@@ -1,16 +1,19 @@
-chrome.storage.local.get(['j_username', 'j_password'], function(items) {
-  
-  if (items.j_username && items.j_password) {
-    $('#q_shib_login')[0].j_username.value = items.j_username;
-    $('#q_shib_login')[0].j_password.value = items.j_password;
+chrome.storage.local.get(['queensUserName', 'queensHashedWord'], function(items) {
+
+  if (items.queensUserName && items.queensHashedWord) {
+    $('#q_shib_login')[0].queensUserName.value = items.queensUserName;
+    $('#q_shib_login')[0].queensHashedWord.value = items.queensHashedWord;
     $('#q_shib_login').submit();
+
   } else {
     $('#q_shib_login').submit(function(event) {
-      var j_username = event.currentTarget.j_username.value;
-      var j_password = event.currentTarget.j_password.value;
+      var queensUserName = event.currentTarget.queensUserName.value;
+      var queensHashedWord = event.currentTarget.queensHashedWord.value;
 
-      chrome.storage.local.set({ j_username: j_username, j_password: j_password });
+      chrome.storage.local.set({
+        queensUserName: queensUserName,
+        queensHashedWord: queensHashedWord
+      });
     });
   }
 });
-
