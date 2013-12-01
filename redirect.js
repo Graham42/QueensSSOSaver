b1c8797b4d3974f5b4fff16a7c9b40373c940f76
Graham McGregor <haxxor42@gmail.com>
2013-11-30 23:33:10 -0500

    reworked to be more flexible

--- redirect.js
+++ redirect.js
@@ -1,5 +1,14 @@
-var notLoggedInStr = "You are not logged";
-if ( $(".logininfo").text().substring(0, notLoggedInStr.length) == notLoggedInStr ){
-  window.location = "https://moodle.queensu.ca/2013-14/login/index.php";
-}
+var notLoggedInStr = "not logged in";
+if ($(".logininfo").text().toLowerCase().indexOf(notLoggedInStr) >= 0) {
 
+  var loginLink = "#"
+  var loginChildren = $(".logininfo").find('a').each(function() {
+    if ($(this).text().toLowerCase().indexOf("login") >= 0) {
+      loginLink = $(this).attr("href");
+      return false;
+    }
+  });
+
+  // should follow something like "https://moodle.queensu.ca/2013-14/login/index.php"
+  window.location = loginLink;
+}
