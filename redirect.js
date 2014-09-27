19a346c4b504c054f6792cdd837f20e48aa7424a
Graham McGregor <dev@grahammcgregor.ca>
2014-09-27 12:37:13 -0400

    Make the text compare of 'Log in' more flexible

--- redirect.js
+++ redirect.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var NOT_LOGGED_IN_TEXT = 'not logged in',
-      LOGIN_TEXT = 'login',
+      LOGIN_RE = /log\s*in/,
       loginInfoDiv = document.querySelector('.logininfo');
 
   if (loginInfoDiv && loginInfoDiv.innerHTML.toLowerCase().indexOf(NOT_LOGGED_IN_TEXT) !== -1) {
@@ -10,7 +10,7 @@
     var loginLink = loginInfoDiv.querySelector('a');
 
     // confirm that this is the Login link
-    if (loginLink && loginLink.text.toLowerCase() === LOGIN_TEXT){
+    if (loginLink && LOGIN_RE.test(loginLink.text.toLowerCase())){
       window.location = loginLink.href || '#';
     }
   }
